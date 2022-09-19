53bca9833f751148039c388aa700f0456046079a
Otire82 <a.jeanne01@sncf.fr>
2022-09-19 16:58:19 +0200

    express8

--- app.js
+++ app.js
@@ -14,24 +14,44 @@ const welcome = (req, res) => {
 
 app.get("/", welcome);
 
+const { hashPassword, verifyPassword, verifyToken } = require("./auth");
+
 const movieHandlers = require("./movieHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", movieHandlers.postMovie);
-app.put("/api/movies/:id", movieHandlers.updateMovie);
-app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+
 
 const userHandlers = require("./userHandlers");
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
+
+app.post(
+  "/api/login",
+  userHandlers.getUserByEmailWithPasswordAndPassToNext,
+  verifyPassword
+);
 app.put("/api/users/:id", userHandlers.updateUser);
 app.delete("/api/users/:id", userHandlers.deleteUser);
 
-const { hashPassword } = require("./auth");
+
+app.post("/api/movies", verifyToken, movieHandlers.postMovie);
+app.put("/api/movies/:id", verifyToken, movieHandlers.updateMovie);
+app.delete("/api/movies/:id", verifyToken, movieHandlers.deleteMovie);
 
 app.post("/api/users" , hashPassword, userHandlers.postUser);
+app.post("/api/movies", verifyToken, movieHandlers.postMovie);
+
+const userIdentified = (req, res) => {
+  if (req.body.email === "dwight@theoffice.com" && req.body.password === "123456") {
+    res.send("Crendentials are valid!!!");
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+app.post("/api/login", userIdentified);
 
 app.listen(port, (err) => {
   if (err) {
